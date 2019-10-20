10925735bdcc9b3bc2707a1bfcf53413a02ba4ec
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2019-10-20 00:01:39 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,67 +1,64 @@
 class sumFunction:
-    def __init__(self,cls):
-        self.tempResult = []
-        self.result = []
+    def __init__(self,cls,seriesNumber):
+        self.currResult = []
+        self.finalResult = []
         self.seriesList = []
+
         for i in range(1,seriesNumber+1):
             self.seriesList.append(i)
 
-        cls.algorithm(self,cls)
+        cls.algorithm(self,cls,seriesNumber)
 
-    def algorithm(self,cls):
-        cls.nextstep(self)
-        cls.step3(self,cls)
-        print("#####",len(self.result),"#####")
-        print(self.result)
+    def algorithm(self,cls,seriesNumber):
+        cls.FirstVals(self,seriesNumber)
+        cls.RemainVals(self,cls,seriesNumber)
+        print(len(self.finalResult))
+        print(self.finalResult)
             
-    def nextstep(self):
+    def FirstVals(self,seriesNumber):
         if seriesNumber%2 == 0:
             for i in range(1,int(seriesNumber/2)):
-                temp1 = []
-                temp2 = []
-                temp1.append(self.seriesList[i])
-                temp2.append(self.seriesList[-i-1])
-                self.tempResult.append((temp1,temp2))
+                tempList1 = []
+                tempList2 = []
+                tempList1.append(self.seriesList[i])
+                tempList2.append(self.seriesList[-i-1])
+                self.currResult.append((tempList1,tempList2))
         else:
             for i in range(1,int((seriesNumber+1)/2)):    
-                temp1 = []
-                temp2 = []
-                temp1.append(self.seriesList[i])
-                temp2.append(self.seriesList[-i-1])
-                if temp1 == temp2:
-                    self.tempResult.append(temp1)
+                tempList1 = []
+                tempList2 = []
+                tempList1.append(self.seriesList[i])
+                tempList2.append(self.seriesList[-i-1])
+                if tempList1 == tempList2:
+                    self.currResult.append(tempList1)
                 else:
-                    self.tempResult.append((temp1,temp2))  
+                    self.currResult.append((tempList1,tempList2))  
         
-        self.result += self.tempResult
+        self.finalResult += self.currResult
 
-    def step3(self,cls):     
-        testList = []
-        for val in self.tempResult:
+    def RemainVals(self,cls,seriesNumber):     
+        usedList = []
+        for val in self.currResult:
             if isinstance(val,tuple):
                 for j in range(2,seriesNumber-val[0][-1]):
-                    self.temp = []
-                    temp1 = val[0]+[val[0][-1]+j]
-                    temp2 = [val[1][0]-j] + val[1]
-                    if temp1 == temp2:
-                        self.temp.append(temp1)
-                        if len(temp1) >= (seriesNumber-2)/2:
-                            self.result += self.temp
+                    tempList = []
+                    tempVal1 = val[0]+[val[0][-1]+j]
+                    tempVal2 = [val[1][0]-j] + val[1]
+                    if tempVal1 == tempVal2:
+                        tempList.append(tempVal1)
+                        if len(tempVal1) >= (seriesNumber-2)/2:
+                            self.finalResult += tempList
                             return False
                         else:                            
-                            self.result += self.temp
-                            self.tempResult += self.temp
+                            self.finalResult += tempList
+                            self.currResult += tempList
                             break
                     else:
-                        if len(temp1) > (seriesNumber-2)/2:
+                        if len(tempVal1) > (seriesNumber-2)/2:
                             return False
-                        elif temp1 in testList:                           
+                        elif tempVal1 in usedList:                           
                             break
-                        self.temp.append((temp1,temp2))
-                        testList.append(temp2)
-                        self.result += self.temp
-                        self.tempResult += self.temp
-
-                        
-for seriesNumber in range(1,13):
-    sumFunction(sumFunction)
+                        tempList.append((tempVal1,tempVal2))
+                        usedList.append(tempVal2)
+                        self.finalResult += tempList
+                        self.currResult += tempList
