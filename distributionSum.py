da88bfd673c160d693388380e4a884d9778d1f76
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2019-10-20 15:34:44 -0400

    Update distributionSum.py

--- distributionSum.py
+++ distributionSum.py
@@ -1,12 +1,6 @@
 from distributionSequence import sumFunction
 
 #Your code starts here:
-
-##peaks(w)#################################################################
-# input: a permutation w
-# output: the peak set of w
-# example: peaks([5,6,1,7,2,4,3]) =[2,4,6]
-###########################################################################
 def peaks(w):
     P=[]
     for i in range(1,len(w)-1):
@@ -14,44 +8,32 @@ def peaks(w):
             P.append(i+1)
     return P
 
-########################
-
-##peak_set(S,n)############################################################
-# input: an integer n and a set S in [n-1]
-# output: all permutations in S_n with peak set S
-# example: peak_set([2],3) = [[1, 3, 2], [2, 3, 1]]
-###########################################################################
 def peak_set(S,n):
-    W = Permutations(n)
+    W = Permutations(range(1,n+1))
     A =[]
     for w in W:
-        #print w
-        #print peaks(w)
         if peaks(w)==S:
             A.append(w)
     return A
 
-##maxlength(P)############################################################
-# input: a set P=P(S;n)
-# output: the maximum length of a permutation in P(S;n)
-# example: P=P([2];4), then maxlength(P) = 9
-###########################################################################
 def maxlength(P):
     L=[]
     for p in P:
         L.append(p.length())
     return max(L)
-                        
-#Your code primarily ends here. However, the code between the ###### below is also from your code.                        
+#Your code primarily ends here. However, the code between the ###### below is also from your code.
 
 
-for seriesNumber in range(1,11):
+for seriesNumber in range(5,8):
     S_list = sumFunction(sumFunction,seriesNumber)
 
     distribList=[]
 
     for S in S_list.finalResult:
         temp=[]
+        
+        #####
+        
         if isinstance(S,tuple):    
             P=peak_set(S[0],seriesNumber)
         else:
@@ -67,7 +49,9 @@ for seriesNumber in range(1,11):
 
         for i in range(maxlength(P)+1):
             temp.append(len(TL[i]))
-
+        
+        #####
+        
         distribList.append(temp)
 
     for i in range(len(distribList)):
@@ -78,12 +62,13 @@ for seriesNumber in range(1,11):
                 break
     
     distribList.sort(key=lambda x:x[int(len(x)/2)])
-    
+
+
     counter=0
     sumList=[]
     
     for iter1 in range(len(distribList)):
-        for iter2 in range(iter1+1,len(distribList)):
+        for iter2 in range(iter1,len(distribList)):
             temp=[]
             for i in range(len(distribList[0])):
                 if distribList[iter1][int(len(distribList[0])/2)]+distribList[iter2][int(len(distribList[0])/2)] > distribList[-1][int(len(distribList[0])/2)]:
@@ -95,14 +80,15 @@ for seriesNumber in range(1,11):
                 sumList.append(temp)
             else:
                 break
-    
+           
     inList=[]
     
     for val in sumList:
         if val in distribList:
             counter += 1
             inList.append(val)
-
+    print(inList)
     print(counter) 
-    
+
+
 # sequence output: 0, 0, 0, 0, 1, 1, 3, 5, 10, 19 ...
